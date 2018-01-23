82585bd5dbd4cb476ea693d0dd6cbf4720bb7e33
chap <chaplonglau@gmail.com>
2018-01-23 10:12:56 -0800

    user sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  get 'pages/about'
   root 'pages#home'
-  resources :users
+  get '/about', to: 'pages#about'
+  get  '/signup', to: 'users#new'
+  post '/signup',  to: 'users#create'
+  resources :users 
 
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
