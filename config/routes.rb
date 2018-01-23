c7f2b329a957930c821cc8c10b9aa91e622a5abe
chap <chaplonglau@gmail.com>
2018-01-23 09:48:29 -0800

    user-setup

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'pages/home'
   get 'pages/about'
   root 'pages#home'
+  resources :users
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
